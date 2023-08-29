a1a08dcccbb8fdf7f84ba329a3214e18595ee56a
GuoJunKe0910 <47590050+GuoJunKe0910@users.noreply.github.com>
2023-08-29 17:46:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,9 +24,9 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wx852c7407a4e03563',
+      id: 'ouj1E6besAyRWwsHPqrIPsIxBHgA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ouj1E6besAyRWwsHPqrIPsIxBHgA',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
